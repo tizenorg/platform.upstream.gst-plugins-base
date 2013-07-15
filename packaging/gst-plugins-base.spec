d224136a86ddf922facbcf50b3ec15ec525b0e2d
Sangchul Lee <sc11.lee@samsung.com>
2013-07-15 14:19:05 +0900

    [xvimagesink] Update value of visible property even if xcontext is null
    Change-Id: Ie4cacbacbe281b38d923db8e0663ef81311749f1

--- packaging/gst-plugins-base.spec
+++ packaging/gst-plugins-base.spec
@@ -1,7 +1,7 @@
 Name:       gst-plugins-base
 Summary:    GStreamer streaming media framework base plug-ins
 Version:    0.10.36
-Release:    29
+Release:    30
 Group:      Applications/Multimedia
 License:    LGPLv2+
 Source0:    %{name}-%{version}.tar.gz
--- sys/xvimage/xvimagesink.c
+++ sys/xvimage/xvimagesink.c
@@ -5573,22 +5573,19 @@ gst_xvimagesink_set_property (GObject * object, guint prop_id,
           }
           XSync( xvimagesink->xcontext->disp, FALSE );
         } else {
-          GST_WARNING_OBJECT( xvimagesink, "xcontext is null, failed to set visible");
-          g_mutex_unlock( xvimagesink->x_lock );
-          g_mutex_unlock( xvimagesink->flow_lock );
-          break;
+          GST_WARNING_OBJECT( xvimagesink, "xcontext is null");
+          xvimagesink->visible = g_value_get_boolean (value);
         }
       } else if (!xvimagesink->visible && (g_value_get_boolean(value) == TRUE)) {
         g_mutex_unlock( xvimagesink->x_lock );
         g_mutex_unlock( xvimagesink->flow_lock );
-        GST_INFO_OBJECT( xvimagesink, "Set visible as TRUE. Update it." );
         xvimagesink->visible = g_value_get_boolean (value);
         gst_xvimagesink_xvimage_put (xvimagesink, xvimagesink->xvimage);
         g_mutex_lock( xvimagesink->flow_lock );
         g_mutex_lock( xvimagesink->x_lock );
       }
 
-      GST_INFO("set visible done");
+      GST_INFO("set visible(%d) done", xvimagesink->visible);
 
       g_mutex_unlock( xvimagesink->x_lock );
       g_mutex_unlock( xvimagesink->flow_lock );
