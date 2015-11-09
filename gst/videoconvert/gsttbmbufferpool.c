63990090d0c90b2a4ba633c93b10f84d0be12dbd
Barun Kumar Singh <barun.singh@samsung.com>
2015-11-09 11:40:44 +0530

    Fixing plane calculation size. Signed-off-by: Barun Kr. Singh <barun.singh@samsung.com> Change-Id: Iee824fe58b34e2ff7bd79cab2621aa7a0e7679d8

--- gst/videoconvert/gsttbmbufferpool.c
+++ gst/videoconvert/gsttbmbufferpool.c
@@ -2,7 +2,7 @@
 #include "gsttbmbufferpool.h"
 #include <gst/video/gstvideofilter.h>
 
-
+#if 0
 int calc_yplane(int width, int height)
 {
     int mbX, mbY;
@@ -22,7 +22,78 @@ int calc_uvplane(int width, int height)
 
     return (ALIGN((mbX * mbY) * 128, 256) + 128);
 }
+#endif
 
+int new_calc_plane(int width, int height)
+{
+    int mbX, mbY;
+
+    mbX = DIV_ROUND_UP(width, S5P_FIMV_NUM_PIXELS_IN_MB_ROW);
+    mbY = DIV_ROUND_UP(height, S5P_FIMV_NUM_PIXELS_IN_MB_COL);
+
+    if (width * height < S5P_FIMV_MAX_FRAME_SIZE)
+      mbY = (mbY + 1) / 2 * 2;
+
+    return ((mbX * S5P_FIMV_NUM_PIXELS_IN_MB_COL) *
+     (mbY * S5P_FIMV_NUM_PIXELS_IN_MB_ROW));
+}
+
+int new_calc_yplane(int width, int height)
+{
+    return (ALIGN_TO_4KB(new_calc_plane(width, height) +
+              S5P_FIMV_D_ALIGN_PLANE_SIZE));
+}
+
+int new_calc_uvplane(int width, int height)
+{
+    return (ALIGN_TO_4KB((new_calc_plane(width, height) >> 1) +
+              S5P_FIMV_D_ALIGN_PLANE_SIZE));
+}
+
+int
+calc_plane(int width, int height)
+{
+    int mbX, mbY;
+
+    mbX = ALIGN(width, S5P_FIMV_NV12MT_HALIGN);
+    mbY = ALIGN(height, S5P_FIMV_NV12MT_VALIGN);
+
+    return ALIGN(mbX * mbY, S5P_FIMV_DEC_BUF_ALIGN);
+}
+
+int
+calc_yplane(int width, int height)
+{
+    int mbX, mbY;
+
+    mbX = ALIGN(width + 24, S5P_FIMV_NV12MT_HALIGN);
+    mbY = ALIGN(height + 16, S5P_FIMV_NV12MT_VALIGN);
+
+    return ALIGN(mbX * mbY, S5P_FIMV_DEC_BUF_ALIGN);
+}
+
+int
+calc_uvplane(int width, int height)
+{
+    int mbX, mbY;
+
+    mbX = ALIGN(width + 16, S5P_FIMV_NV12MT_HALIGN);
+    mbY = ALIGN(height + 4, S5P_FIMV_NV12MT_VALIGN);
+
+    return ALIGN(mbX * mbY, S5P_FIMV_DEC_BUF_ALIGN);
+}
+
+int
+gst_calculate_y_size(int width, int height)
+{
+   return CHOOSE_MAX_SIZE(calc_yplane(width,height),new_calc_yplane(width,height));
+}
+
+int
+gst_calculate_uv_size(int width, int height)
+{
+   return CHOOSE_MAX_SIZE(calc_uvplane(width,height),new_calc_uvplane(width,height));
+}
 
 static GstMemory *
 gst_mm_memory_allocator_alloc_dummy (GstAllocator * allocator, gsize size,
@@ -255,20 +326,26 @@ gst_mm_buffer_pool_alloc_buffer (GstBufferPool * bpool,
     height = GST_VIDEO_INFO_HEIGHT (&pool->video_info);
     mm_data = g_malloc0(sizeof(MMVideoBuffer));
     mm_video_buf = (MMVideoBuffer*) mm_data;
-    mm_video_buf->type = MM_VIDEO_BUFFER_TYPE_TBM_BO;
+    mm_video_buf->type = MM_VIDEO_BUFFER_TYPE_DMABUF_FD;
     mm_video_buf->plane_num = 2;
     /* Setting Y plane size */
-    mm_video_buf->size[0] = calc_yplane(width, height);
+    mm_video_buf->size[0] = gst_calculate_y_size(width, height);
     /* Setting UV plane size */
-    mm_video_buf->size[1] = calc_uvplane(width, height);
+    mm_video_buf->size[1] = gst_calculate_uv_size(width, height);
     mm_video_buf->handle.bo[0] = tbm_bo_alloc(pool->hTBMBufMgr, mm_video_buf->size[0], TBM_BO_WC);
     mm_video_buf->handle.bo[1] = tbm_bo_alloc(pool->hTBMBufMgr, mm_video_buf->size[1], TBM_BO_WC);
 
     mm_video_buf->handle.dmabuf_fd[0] = (tbm_bo_get_handle(mm_video_buf->handle.bo[0], TBM_DEVICE_MM)).u32;
     mm_video_buf->handle.dmabuf_fd[1] = (tbm_bo_get_handle(mm_video_buf->handle.bo[1], TBM_DEVICE_MM)).u32;
 
+    /*
     mm_video_buf->handle.paddr[0] = (tbm_bo_map(mm_video_buf->handle.bo[0], TBM_DEVICE_CPU,TBM_OPTION_WRITE)).ptr;
     mm_video_buf->handle.paddr[1] = (tbm_bo_map(mm_video_buf->handle.bo[1], TBM_DEVICE_CPU,TBM_OPTION_WRITE)).ptr;
+    */
+
+    mm_video_buf->data[0] = (tbm_bo_map(mm_video_buf->handle.bo[0], TBM_DEVICE_CPU,TBM_OPTION_WRITE)).ptr;
+    mm_video_buf->data[1] = (tbm_bo_map(mm_video_buf->handle.bo[1], TBM_DEVICE_CPU,TBM_OPTION_WRITE)).ptr;
+
     /* Setting stride height & width for Y plane */
     mm_video_buf->stride_height[0] = mm_video_buf->height[0] = height;
     mm_video_buf->stride_width[0] = mm_video_buf->width[0] = width;
